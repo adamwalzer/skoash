ba2c1210810b6ad56cceab631063b9b90196649c
Adam Walzer <adamwalzer@gmail.com>
2017-02-22 17:00:59 -0500

    load json fix

--- src/methods/load_json.js
+++ src/methods/load_json.js
@@ -1,5 +1,6 @@
 import loadFile from './load_file.js';
 
 export default function (file, callback) {
-    return loadFile(file, callback, 'json');
+    let data = loadFile(file, callback, 'json');
+    return _.isString(data) ? JSON.parse(data) : data;
 }
